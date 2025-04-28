562c0893cf60f41359b68463ba8aa72fa85fc79a
Nozito <tekmanpro@gmail.com>
2025-04-29 01:25:14 +0200

    feat: remplacer les balises img par le composant Image de Next.js dans la page À propos et la Navbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Footer } from '../components/Footer';
 import { Navbar } from '../components/Navbar';
 import '../globals.css';
+import Image from 'next/image';
 
 export default function About() {
   return (
@@ -12,7 +13,9 @@ export default function About() {
       <Navbar />
 
       {/* Hero Section */}
-      <title>À PROPOS</title>
+      <head>
+        <title>À PROPOS</title>
+      </head>
       <div className="flex justify-center items-center flex-col h-[50vh] bg-black">
         <h1 className="text-5xl font-bold mb-4">À PROPOS</h1>
       </div>
@@ -21,53 +24,52 @@ export default function About() {
       <section className="container mx-auto py-12 px-6">
         <div className="flex flex-col md:flex-row items-center md:items-start">
           {/* Profile Photo */}
-          <div className="flex-shrink-0 mb-8 md:mb-0 md:mr-8">
-            <img
-              src="img/Noah.jpeg" // Vérifiez que l'image est bien placée dans le dossier public/img
-              alt="Photo de profil"
-              className="rounded-lg shadow-lg w-72 h-[28rem] object-cover" // Augmentation de la hauteur
-            />
-          </div>
+          <Image
+            src="/img/Noah.jpeg"
+            alt="Photo de profil"
+            width={288}
+            height={448} 
+            className="rounded-lg shadow-lg w-72 h-[28rem] object-cover"
+          />
+        </div>
 
-          {/* Profile Text */}
-          <div className="flex-1 text-lg leading-relaxed">
-            <h2 className="text-4xl font-semibold mb-4">Bonjour,</h2>
-            <p className="mb-4">
-              Je suis <strong>Noah</strong>, photographe et vidéaste amateur, j'étudie en parallèle en BTS Informatique...
-            </p>
-            <p className="mb-4">
-              Avant de prendre mon indépendance en photos et en vidéos, j’ai suivi pendant 3 ans, en parallèle de mes
-              cours, une option cinéma-audiovisuel. J’ai pu réaliser, avec des collègues, 3 courts métrages, ainsi que
-              d’autres projets que vous retrouverez dans mon portfolio.
-            </p>
-            <p className="mb-4">
-              À travers ce portfolio, vous pourrez suivre mon évolution depuis mes débuts, que ce soit en photo ou en
-              cinématographie.
-            </p>
-            <p className="mb-4">
-              Je n'ai pas de spécialisation particulière en matière de photographie. Je préfère explorer divers sujets
-              pour rester polyvalent. Ainsi, vous découvrirez une variété de photos, toutes animées par ma passion. Ces
-              images reflètent mon intérêt pour la photographie, et vous invitent à plonger dans leur univers comme si
-              vous en faisiez partie !
-            </p>
-            <p className="mb-4">
-              Merci de prendre le temps de découvrir mon travail. J'espère que ces images vous transporteront et vous
-              inspireront autant qu'elles m'inspirent.
-            </p>
-            <p className="mb-4">
-              Voici aussi mon{' '}
-              <a
-                href="http://127.0.0.1:5501"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline hover:text-blue-400"
-              >
-                Portfolio de Développeur
-              </a>
-              .
-            </p>
-            <p className="font-semibold">Noah</p>
-          </div>
+        {/* Profile Text */}
+        <div className="flex-1 text-lg leading-relaxed">
+          <h2 className="text-4xl font-semibold mb-4">Bonjour,</h2>
+          <p className="mb-4">
+            Je suis <strong>Noah</strong>, photographe et vidéaste amateur, j'étudie en parallèle en BTS Informatique...
+          </p>
+          <p className="mb-4">
+            Avant de prendre mon indépendance en photos et en vidéos, j’ai suivi pendant 3 ans, en parallèle de mes
+            cours, une option cinéma-audiovisuel. J’ai pu réaliser, avec des collègues, 3 courts métrages, ainsi que
+            d’autres projets que vous retrouverez dans mon portfolio.
+          </p>
+          <p className="mb-4">
+            À travers ce portfolio, vous pourrez suivre mon évolution depuis mes débuts, que ce soit en photo ou en
+            cinématographie.
+          </p>
+          <p className="mb-4">
+            Je n'ai pas de spécialisation particulière en matière de photographie. Je préfère explorer divers sujets
+            pour rester polyvalent. Ainsi, vous découvrirez une variété de photos, toutes animées par ma passion. Ces
+            images reflètent mon intérêt pour la photographie, et vous invitent à plonger dans leur univers comme si
+            vous en faisiez partie !
+          </p>
+          <p className="mb-4">
+            Merci de prendre le temps de découvrir mon travail. J'espère que ces images vous transporteront et vous
+            inspireront autant qu'elles m'inspirent.
+          </p>
+          <p className="mb-4">
+            Voici aussi mon{' '}
+            <a
+              href="dekeyzer.fr"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline hover:text-blue-400"
+            >
+              Portfolio de Développeur
+            </a>
+            .
+          </p>
         </div>
       </section>
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export function Navbar() {
     const [isPortfolioOpen, setIsPortfolioOpen] = useState(false);
@@ -14,13 +16,13 @@ export function Navbar() {
             {/* Desktop Navbar */}
             <div className="container mx-auto px-4 py-2 hidden md:flex flex-row justify-between items-center">
                 {/* Logo */}
-                <a href="/" className="mb-4 md:mb-0">
-                    <img src="img/Nb.png" alt="Noah_pfp" className="h-24 w-24" />
-                </a>
+                <Link href="/" className="mb-4 md:mb-0">
+                    <Image src="/img/Nb.png" alt="Noah_pfp" className="h-24 w-24" width={96} height={96} />
+                </Link>
 
                 {/* Navigation Links */}
                 <div className="hidden md:flex space-x-8 text-white">
-                    <a href="/" className="hover:text-gray-400">Accueil</a>
+                    <Link href="/" className="hover:text-gray-400">Accueil</Link>
                     <div className="relative">
                         <button
                             className="hover:text-gray-400"
@@ -30,28 +32,28 @@ export function Navbar() {
                         </button>
                         {isPortfolioOpen && (
                             <ul className="absolute bg-black text-white mt-2 space-y-2 p-2 rounded-md shadow-lg w-40 z-10">
-                                <li><a href="concerts" className="block py-2 px-4 hover:bg-gray-700">Concerts</a></li>
-                                <li><a href="voitures" className="block py-2 px-4 hover:bg-gray-700">Voitures</a></li>
-                                <li><a href="portraits" className="block py-2 px-4 hover:bg-gray-700">Portraits</a></li>
-                                <li><a href="voyages" className="block py-2 px-4 hover:bg-gray-700">Voyages</a></li>
+                                <li><Link href="concerts" className="block py-2 px-4 hover:bg-gray-700">Concerts</Link></li>
+                                <li><Link href="voitures" className="block py-2 px-4 hover:bg-gray-700">Voitures</Link></li>
+                                <li><Link href="portraits" className="block py-2 px-4 hover:bg-gray-700">Portraits</Link></li>
+                                <li><Link href="voyages" className="block py-2 px-4 hover:bg-gray-700">Voyages</Link></li>
                             </ul>
                         )}
                     </div>
-                    <a href="about" className="hover:text-gray-400">À propos</a>
-                    <a href="contact" className="hover:text-gray-400">Contact</a>
+                    <Link href="about" className="hover:text-gray-400">À propos</Link>
+                    <Link href="contact" className="hover:text-gray-400">Contact</Link>
                 </div>
 
                 {/* Social Media Icons (Desktop only) */}
                 <div className="hidden md:flex items-center space-x-2">
-                    <a href="https://www.instagram.com/noahdkr_" className="text-white hover:text-gray-400">
-                        <img src="img/instagram.png" alt="instagram" className="h-7 w-7" />
-                    </a>
-                    <a href="https://www.tiktok.com/@noahdkr_" className="text-white hover:text-gray-400">
-                        <img src="img/tiktok.png" alt="tiktok" className="h-7 w-7" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/noah-dkr/" className="text-white hover:text-gray-400">
-                        <img src="img/linkedin.png" alt="linkedin" className="h-7 w-7" />
-                    </a>
+                    <Link href="https://www.instagram.com/noahdkr_" className="text-white hover:text-gray-400">
+                        <Image src="/img/instagram.png" alt="instagram" className="h-7 w-7" width={28} height={28} />
+                    </Link>
+                    <Link href="https://www.tiktok.com/@noahdkr_" className="text-white hover:text-gray-400">
+                        <Image src="/img/tiktok.png" alt="tiktok" className="h-7 w-7" width={28} height={28} />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/noah-dkr/" className="text-white hover:text-gray-400">
+                        <Image src="/img/linkedin.png" alt="linkedin" className="h-7 w-7" width={28} height={28} />
+                    </Link>
                 </div>
             </div>
 
@@ -59,9 +61,9 @@ export function Navbar() {
             <div className="md:hidden flex justify-between items-center px-4 py-2">
                 {/* Logo centered */}
                 <div className="flex-1 text-center">
-                    <a href="/">
-                        <img src="img/Nb.png" alt="Noah_pfp" className="h-20 w-20 mx-auto" />
-                    </a>
+                    <Link href="/">
+                        <Image src="/img/Nb.png" alt="Noah_pfp" className="h-20 w-20 mx-auto" width={80} height={80} />
+                    </Link>
                 </div>
 
                 {/* Burger Menu Button */}
@@ -75,34 +77,34 @@ export function Navbar() {
             {/* Mobile Menu Dropdown */}
             {isMobileMenuOpen && (
                 <div className="md:hidden bg-black text-white px-6 py-4 space-y-4">
-                    <a href="/" className="block hover:text-gray-400">Accueil</a>
+                    <Link href="/" className="block hover:text-gray-400">Accueil</Link>
                     <div>
                         <button onClick={togglePortfolio} className="hover:text-gray-400">Galerie</button>
                         {isPortfolioOpen && (
                             <ul className="mt-2 space-y-2 ml-4">
-                                <li><a href="concerts" className="block hover:text-gray-400">Concerts</a></li>
-                                <li><a href="voitures" className="block hover:text-gray-400">Voitures</a></li>
-                                <li><a href="portraits" className="block hover:text-gray-400">Portraits</a></li>
-                                <li><a href="voyages" className="block hover:text-gray-400">Voyages</a></li>
+                                <li><Link href="concerts" className="block hover:text-gray-400">Concerts</Link></li>
+                                <li><Link href="voitures" className="block hover:text-gray-400">Voitures</Link></li>
+                                <li><Link href="portraits" className="block hover:text-gray-400">Portraits</Link></li>
+                                <li><Link href="voyages" className="block hover:text-gray-400">Voyages</Link></li>
                             </ul>
                         )}
                     </div>
-                    <a href="about" className="block hover:text-gray-400">À propos</a>
-                    <a href="contact" className="block hover:text-gray-400">Contact</a>
+                    <Link href="about" className="block hover:text-gray-400">À propos</Link>
+                    <Link href="contact" className="block hover:text-gray-400">Contact</Link>
 
                     <hr className="border-gray-600 my-4" />
 
                     {/* Social icons (mobile only) */}
                     <div className="flex justify-center space-x-4">
-                        <a href="https://www.instagram.com/noahdkr_">
-                            <img src="img/instagram.png" alt="instagram" className="h-7 w-7" />
-                        </a>
-                        <a href="https://www.tiktok.com/@noahdkr_">
-                            <img src="img/tiktok.png" alt="tiktok" className="h-7 w-7" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/noah-dkr/">
-                            <img src="img/linkedin.png" alt="linkedin" className="h-7 w-7" />
-                        </a>
+                        <Link href="https://www.instagram.com/noahdkr_">
+                            <Image src="/img/instagram.png" alt="instagram" className="h-7 w-7" width={28} height={28} />
+                        </Link>
+                        <Link href="https://www.tiktok.com/@noahdkr_">
+                            <Image src="/img/tiktok.png" alt="tiktok" className="h-7 w-7" width={28} height={28} />
+                        </Link>
+                        <Link href="https://www.linkedin.com/in/noah-dkr/">
+                            <Image src="/img/linkedin.png" alt="linkedin" className="h-7 w-7" width={28} height={28} />
+                        </Link>
                     </div>
                 </div>
             )}
